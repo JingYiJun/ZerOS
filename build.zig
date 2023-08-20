0e550cd304371338cb9e2587f09e6cbabc1a3971
JingYiJun <jingyijun3104@outlook.com>
2023-08-20 19:42:55 +0800

    init: generate boot.img

--- build.zig
+++ build.zig
@@ -4,6 +4,9 @@ const Model = Target.Cpu.Model;
 const Feature = Target.Cpu.Feature;
 const CrossTarget = std.zig.CrossTarget;
 const fs = std.fs;
+const allocPrint = std.fmt.allocPrint;
+const comptimePrint = std.fmt.comptimePrint;
+const StringList = std.ArrayList([]const u8);
 
 pub fn build(b: *std.Build) !void {
     const target = CrossTarget{
@@ -63,14 +66,18 @@ pub fn build(b: *std.Build) !void {
         "--all-headers",
         kernel_elf_path,
     });
+    kernel_hdr.step.dependOn(b.getInstallStep());
 
     // relocate stdout and stderr to files
     const kernel_asm_output = b.addWriteFiles();
-    _ = kernel_asm_output.addCopyFile(kernel_asm.captureStdOut(), kernel_asm_path);
-    _ = kernel_asm_output.addCopyFile(kernel_asm.captureStdErr(), kernel_asm_error_path);
-    _ = kernel_asm_output.addCopyFile(kernel_hdr.captureStdOut(), kernel_hdr_path);
     kernel_asm_output.step.dependOn(&kernel_asm.step);
     kernel_asm_output.step.dependOn(&kernel_hdr.step);
+    kernel_asm_output.addCopyFileToSource(kernel_asm.captureStdOut(), kernel_asm_path);
+    kernel_asm_output.addCopyFileToSource(kernel_asm.captureStdErr(), kernel_asm_error_path);
+    kernel_asm_output.addCopyFileToSource(kernel_hdr.captureStdOut(), kernel_hdr_path);
+
+    const kernel_asm_step = b.step("kernel-asm", "output-kernel-asm");
+    kernel_asm_step.dependOn(&kernel_asm_output.step);
 
     // build kernel.img
     const kernel_img_name = kernel_name ++ ".img";
@@ -86,9 +93,72 @@ pub fn build(b: *std.Build) !void {
     });
     kernel_img.step.dependOn(b.getInstallStep());
 
+    // build fs.img
+    // first: generate fs.img and make fat32 filesystem
+    var fs_files = StringList.init(b.allocator);
+    try fs_files.appendSlice(&.{
+        kernel_img_path,
+        "armstub8-rpi4.bin",
+        "bootcode.bin",
+        "config.txt",
+        "fixup_cd.dat",
+        "fixup.dat",
+        "fixup4.dat",
+        "fixup4cd.dat",
+        "LICENCE.broadcom",
+        "start_cd.elf",
+        "start.elf",
+        "start4.elf",
+        "start4cd.elf",
+    });
+    const boot_img_name = "boot.img";
+    const boot_img_path = b.getInstallPath(.bin, boot_img_name);
+    const n_boot_sectors = 128 * 1024;
+    const sector_size = 512;
+
+    var generate_boot_img = b.addSystemCommand(&.{
+        "dd",
+        "if=/dev/zero",
+        try allocPrint(b.allocator, "of={s}", .{boot_img_path}),
+        try allocPrint(b.allocator, "seek={d}", .{n_boot_sectors - 1}),
+        try allocPrint(b.allocator, "bs={d}", .{sector_size}),
+        "count=1",
+    });
+
+    var generate_boot_img_fs = b.addSystemCommand(&.{
+        "mkfs.vfat",
+        "-F",
+        "32",
+        "-s",
+        "1",
+        boot_img_path,
+    });
+    generate_boot_img_fs.step.dependOn(&generate_boot_img.step);
+
+    // copy files into boot partition
+    const copy_files_into_boot_partition_step = b.step("copy_files", "copy_files");
+    copy_files_into_boot_partition_step.dependOn(&generate_boot_img_fs.step);
+    for (fs_files.items) |file| {
+        const copy_file = b.addSystemCommand(&.{
+            "mcopy",
+            "-i",
+            boot_img_path,
+            file,
+            try allocPrint(b.allocator, "::{s}", .{fs.path.basename(file)}),
+        });
+        copy_file.cwd = "boot";
+        copy_file.step.dependOn(&generate_boot_img_fs.step);
+        if (std.mem.eql(u8, fs.path.basename(file), kernel_img_name)) {
+            copy_file.step.dependOn(&kernel_img.step);
+        }
+        copy_files_into_boot_partition_step.dependOn(&copy_file.step);
+    }
+
     // start qemu if use `zig build qemu`
     const qemu_step = b.step("qemu", "Run ZerOS in qemu");
-    const qemu_command = b.addSystemCommand(&.{
+    var qemu_args = std.ArrayList([]const u8).init(b.allocator);
+    defer qemu_args.deinit();
+    try qemu_args.appendSlice(&.{
         "qemu-system-aarch64",
         "--machine",
         "raspi3b",
@@ -100,29 +170,26 @@ pub fn build(b: *std.Build) !void {
         "-kernel",
         kernel_img_path,
     });
+    var current_qemu_args = try qemu_args.clone();
+    const qemu_command = b.addSystemCommand(try current_qemu_args.toOwnedSlice());
 
     qemu_command.step.dependOn(&kernel_img.step);
     qemu_command.step.dependOn(&kernel_asm_output.step);
+    qemu_command.step.dependOn(copy_files_into_boot_partition_step);
     qemu_step.dependOn(&qemu_command.step);
 
     // start qemu debug
     const qemu_debug_step = b.step("qemu-debug", "Run ZerOS in qemu with gdb");
-    const qemu_debug_command = b.addSystemCommand(&.{
-        "qemu-system-aarch64",
-        "--machine",
-        "raspi3b",
-        "-nographic",
-        "-serial",
-        "null",
-        "-serial",
-        "mon:stdio",
-        "-kernel",
-        kernel_img_path,
+    current_qemu_args = try qemu_args.clone();
+    try current_qemu_args.appendSlice(&.{
         "-gdb",
         "tcp::1234",
         "-S",
     });
+    const qemu_debug_command = b.addSystemCommand(try current_qemu_args.toOwnedSlice());
     qemu_debug_command.step.dependOn(&kernel_img.step);
+    qemu_debug_command.step.dependOn(&kernel_asm_output.step);
+    qemu_debug_command.step.dependOn(copy_files_into_boot_partition_step);
     qemu_debug_step.dependOn(&qemu_debug_command.step);
 
     // uninstall step
@@ -133,6 +200,7 @@ pub fn build(b: *std.Build) !void {
     b.pushInstalledFile(.bin, kernel_asm_name);
     b.pushInstalledFile(.bin, kernel_asm_error);
     b.pushInstalledFile(.bin, kernel_hdr_name);
+    b.pushInstalledFile(.bin, boot_img_name);
 
     // clean step
     // remove zig-cache and `prefix path`
